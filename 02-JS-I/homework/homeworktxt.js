ba24bffd71a3daf6671266586fbd435d2b944a42
Luna Heuguerot <lunaheuguerot@gmail.com>
2022-12-14 19:18:04 -0300

    actualizacion 14-12 js

--- 02-JS-I/homework/homeworktxt.js
+++
@@ -1,5 +0,0 @@
-// Variables: sirven para guardar un código o valor en un lugar donde pueda recuperarlo.
-// Strings: es un hilo de caracteres delimitado por comillas.
-// Funciones: nos permiten ejecutar y usar códigos cuantas veces quiera, al ejecutarlas hacen y a su vez retornan algo. Para que retorne se utiliza la palabra return. Los argumentos dentro de la función son variables que van a tener un valor cuando se invoque la función, van separados de comas. 
-// Declaraciones: las declaraciones realizan una acción, se utilizan condicionales ('if', 'else', 'else if') para ejecutar un código u otro. 
-// Valores booleanos: son valores de lógica binaria (verdadero o falso), un dato que tiene dos opciones posibles. 
